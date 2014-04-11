03448fd042be1ec0e97408e82a5e15d218f0d594
Phillip Baker <phillip@digitalocean.com>
2014-04-11 11:26:27 -0400

    Initial commit - structure of basic gem.

---
+++ lib/omniauth/digitalocean.rb
@@ -0,0 +1,7 @@
+require "omniauth/digitalocean/version"
+
+module Omniauth
+  module Digitalocean
+    # Your code goes here...
+  end
+end
---
+++ lib/omniauth/digitalocean/version.rb
@@ -0,0 +1,5 @@
+module Omniauth
+  module Digitalocean
+    VERSION = "0.0.1"
+  end
+end
