000da808cc1a1c425bffa8d8fe97cb5851c4ba4d
Phillip Baker <phillip@digitalocean.com>
2015-09-02 10:22:40 -0400

    Pull uid from uuid in info hash.
    This updates this client for changes as of https://developers.digitalocean.com/documentation/changelog/oauth/add-oauth-uuid/.

--- spec/omniauth/strategies/digitalocean_spec.rb
+++ spec/omniauth/strategies/digitalocean_spec.rb
@@ -5,7 +5,7 @@ describe OmniAuth::Strategies::Digitalocean do
     described_class.new({})
   end
 
-  context "client options" do
+  describe "production client options" do
     it { expect(subject.options.name).to eq(:digitalocean) }
 
     it { expect(subject.options.client_options.site).to eq("https://cloud.digitalocean.com") }
@@ -13,4 +13,25 @@ describe OmniAuth::Strategies::Digitalocean do
     it { expect(subject.options.client_options.token_url).to eq("https://cloud.digitalocean.com/v1/oauth/token") }
   end
 
+  describe "callback phase instance methods" do
+    let(:uuid) { 123 }
+    let(:response_params) {
+      {
+        'info' => {
+          'uuid' => uuid
+        }
+      }
+    }
+    let(:access_token) { double('AccessToken', params: response_params) }
+
+    before do
+      allow(subject).to receive(:access_token).and_return(access_token)
+    end
+
+    describe "#uid" do
+      it "returns uuid from the info hash" do
+        expect(subject.uid).to eq(uuid)
+      end
+    end
+  end
 end
